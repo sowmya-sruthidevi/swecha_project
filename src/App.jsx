7dc3481caf6ebe27115f4d3285b62bcd133d4298
jnanesh250 <jnaneshmadupalli@gmail.com>
2026-09-13 02:31:00 +0530

    Completely remove chatbot service, component, and styles

--- src/App.jsx
+++ src/App.jsx
@@ -11,12 +11,9 @@ import GroupDetails from './pages/GroupDetails.jsx';
 import Profile from './pages/Profile.jsx';
 import Notifications from './pages/Notifications.jsx';
 import ProtectedRoute from './components/ProtectedRoute.jsx';
-import ChatbotLauncher from './components/ChatbotLauncher.jsx';
 
 function App() {
-  
-return (
-  <>
+  return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/signup" element={<Signup />} />
@@ -96,10 +93,7 @@ return (
 
       <Route path="*" element={<Home />} />
     </Routes>
-
-    <ChatbotLauncher />
-  </>
-);
+  );
 }
+
 export default App;
-  
