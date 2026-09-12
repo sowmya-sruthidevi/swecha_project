6e4ff332c29214077bf72ed50c370224a7983067
Sowmya Sruthi Devi Nimmala <sruthinimmalaa206@gmail.com>
2026-09-12 18:19:33 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,16 +12,14 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://127.0.0.1:3000', 'http://localhost:3001', 'http://127.0.0.1:3001',app.use(cors({
   origin: [
     'http://localhost:3000',
     'http://127.0.0.1:3000',
     'http://localhost:3001',
     'http://127.0.0.1:3001',
-     'https://swecha-project-ten.vercel.app' ],
-  credentials: true,
-}));],
-  credentials: true,
+    'https://swecha-project-ten.vercel.app'
+  ],
+  credentials: true
 }));
 
 app.use(express.json({ limit: '50mb' }));
